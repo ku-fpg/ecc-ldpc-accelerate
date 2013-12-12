e4264f5ce1666c573f218d27d530b6800e1ade69
Andy Gill <andygill@ku.edu>
2013-12-12 14:50:18 -0600

    First decoder in accelerate

--- src/ECC/Code/LDPC/Accelerate.hs
+++ src/ECC/Code/LDPC/Accelerate.hs
@@ -13,15 +13,17 @@ import qualified Data.Matrix
 import Data.Matrix ((!))
 import Data.Matrix (nrows, ncols, getCol, getRow, colVector, rowVector)
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
+
 import Data.Semigroup ((<>), Monoid, mconcat)
 
 import Data.Array.Accelerate.Interpreter as I
-import Data.Array.Accelerate hiding ((++), product, take, all, (!))
+import Data.Array.Accelerate hiding ((++), product, take, all, (!), fst, snd, zipWith, not, zip, or, map)
 import qualified Data.Array.Accelerate as A
 
-import qualified Data.Array.Accelerate.Array.Sugar as Sugar
-import qualified Data.Array.Accelerate.Array.Data as Data
-import qualified Data.Array.Accelerate.Type as Type
+--import qualified Data.Array.Accelerate.Array.Sugar as Sugar
+--import qualified Data.Array.Accelerate.Array.Data as Data
+--import qualified Data.Array.Accelerate.Type as Type
 import Debug.Trace
 import Data.Typeable
 import Data.Word
@@ -30,7 +32,11 @@ import Data.Matrix (Matrix)
 import qualified Data.Matrix as M
 
 import qualified ECC.Code.LDPC.Reference as Ref
+import ECC.Code.LDPC.Model (Decoder(..), Share(..), share_minsum)
 import qualified ECC.Code.LDPC.Model as Mod
+import qualified Data.BitMatrix.Word64 as BM64
+import qualified Data.BitVector.Word64 as BV64
+
 
 code :: Code
 code = mconcat [  mkLDPC_Code ("accel-" ++ show i ++ "-" ++ show j) e d
@@ -39,7 +45,7 @@ code = mconcat [  mkLDPC_Code ("accel-" ++ show i ++ "-" ++ show j) e d
                ]
 
 encoders = [Mod.encoder4,encoder]
-decoders = [Mod.decoder Mod.decoder8]
+decoders = [Mod.decoder Mod.decoder8,Mod.decoder decoder_acc1]
 
 decoder :: Matrix Bit -> Int -> [Double] -> IO [Bit]
 decoder = Mod.decoder Mod.decoder8
@@ -91,97 +97,107 @@ takeRow n mat =
                  (\ix -> index2 n (unindex1 ix))
                  mat
 
-{-
+
+
+-- First cut at using Accelerate
+decoder_acc1 = Decoder
+        { pre_a        =  \ h ->
+                                let vs = [ (m,n) | n <- [1..ncols h], m <- [1..nrows h], h ! (m,n) == 1 ] in
+                                ( h
+                                        -- The bit vector for the parity check
+                                , BM64.fromLists [[ h ! (m,n) | n <- [1..ncols h]] | m <- [1..nrows h]]
+                                        -- all the left/right neibours
+                                , U.fromList vs
+                                )
+        , pre_lambda   = U.fromList
+        , check_parity =  \ (m_opt,m,_) lam -> not $ or $ BM64.parityMatVecMul m (BV64.fromList (fmap hard (U.toList lam)))
+        , post_lambda  =  map hard . U.toList
+        , pre_ne       = \ (m_opt,_,mns) -> U.map (const 0) mns
+        , comp_ne      = \  share (m_opt,_,mns) lam ne ->
+
+                let vs = [ (m,n) | n <- [1..ncols m_opt], m <- [1..nrows m_opt], m_opt ! (m,n) == 1 ] in
+
+                -- msA and nsA are (-1)'d to index into an array better
+                let msA = use $ A.fromList (Z :. length vs) (map (pred . fst) vs) :: Acc (Array DIM1 Int) in
+                let nsA = use $ A.fromList (Z :. length vs) (map (pred . snd) vs) :: Acc (Array DIM1 Int) in
+                let neA = use $ A.fromList (Z :. length vs) (U.toList ne) :: Acc (Array DIM1 Double) in
+                let lamA = use $ A.fromList (Z :. U.length lam) (U.toList lam) :: Acc (Array DIM1 Double) in
+
+                -- The new way
+                -- Flat array of values
+
+                let interm_arrA = A.zipWith (\ n v -> - ((lamA A.! index1 n) - v)) nsA neA in
+
+                let interm_arr = U.fromList (A.toList (run interm_arrA)) in
+                let interm_arr' = U.zipWith (\ (_,n) v -> - ((lam U.! (n-1)) - v)) mns ne in
+
+                let falseA = A.generate (index1 (lift (nrows m_opt))) (\ _ -> lift False) in
+
+                let signA = A.permute (/=*)
+                                      falseA    -- all +ve
+                                      (\ ix -> index1 (msA A.! ix))
+                                      (A.map (<* 0) (interm_arrA)) :: Acc (Array DIM1 Bool) in
+
+
+                let sign' = U.accumulate (\ a b -> if b < 0 then not a else a)
+                                   (U.generate (nrows m_opt) (const False))
+                                   (U.zip (U.map (pred . fst) mns) interm_arr) in
+
+                let sign = U.zipWith (\ b1 b2 -> if b1 == b2 then b2 else (traceShow (b1,b2) b1))
+                                     sign'
+                                     (U.fromList (A.toList (run signA))) in
+
+                let val' = U.accumulate (\ (b,c) a -> if abs a <= b
+                                                     then (abs a,b)
+                                                     else (b,min (abs a) c))
+                                   (U.generate (nrows m_opt) (const (1/0,1/0)))     -- IEEE magic
+                                   (U.zip (U.map (pred . fst) mns) interm_arr) in
+
+                let infsA = A.generate (index1 (lift (length vs))) (\ _ -> lift (1/0::Double,1/0::Double)) in
+
+                let valA = A.permute
+                                (\ a12 b12 -> let (a1,a2) = unlift a12
+                                                  (b1,b2) = unlift b12
+                                              in (a1 <=* b1)
+                                               ? ( lift (a1, min a2 b1)
+                                                 , lift (b1, min b2 a1)
+                                                 ))
+                                infsA
+                                (\ ix -> index1 (msA A.! ix))
+                                (A.map (\ v -> lift (abs v,1/0::Double)) interm_arrA) :: Acc (Array DIM1 (Double,Double)) in
+
+                let val = U.zipWith (\ a b -> if a == b then a else traceShow (a,b) a)
+                                    val'
+                                     (U.fromList (A.toList (run valA))) in
+
+                let ans2 = U.zipWith (\ (m,_) v ->
+                                        let sgn = if sign U.! (m-1) == (v < 0) then 1 else -1 in
+                                        let (a,b) = val U.! (m-1) in
+                                        if a == abs v
+                                        then (-0.75) * sgn * b
+                                        else (-0.75) * sgn * a
+                                     ) mns interm_arr in
+
+                let ans2A = A.zipWith (\ m v ->
+
+                                        let sgn = ((signA A.! index1 m) ==* (v <* 0)) ? (1,-1) :: Exp Double in
+                                        let (a,b) = unlift (valA A.! index1 m) :: (Exp Double,Exp Double) in
+                                        (a ==* abs v) ?
+                                           ( (-0.75) * sgn * b
+                                           , (-0.75) * sgn * a
+                                           ) :: Exp Double
+                                     ) msA interm_arrA in
+
+                (U.zipWith (\ a b -> if a == b then a else traceShow (a,b) b)
+                                        (U.fromList (A.toList (run ans2A)))
+                                        ans2)
+--                (traceShow comp ans1)
+        , comp_lam     = \ (m_opt,_,mns) orig_lam ne' ->
+                U.accum (+) orig_lam [ (n-1,v) | ((_,n),v) <- U.toList mns `zip` U.toList ne' ]
+        , share = share_minsum :: Share Double [(Int,Double)] Int -- ignored
+        }
+
 --encoder :: (Elt e, IsNum e) => G -> V e -> V e
 --encoder g v = mvm g v
 
 
-instance Elt Bit where
-  eltType _ = Type.PairTuple Type.UnitTuple (Type.SingleTuple Type.scalarType)
-  fromElt v = ((), Sugar.fromElt' v)
-  toElt ((), v) = Sugar.toElt' v
-
-  eltType' _ = Type.SingleTuple Type.scalarType
-  fromElt' v = if getBit v then 1 else 0
-  toElt' 0 = 0
-  toElt' 1 = 1
-  toElt' _ = error "toElt' _ :: Bit"
-
-type instance Sugar.EltRepr' Bit = Word8
-type instance Sugar.EltRepr Bit = ((),Word8)
-
-{-
-instance Data.ArrayElt Bit where
--}
-
---instance Type.IsBounded Bit where
---  boundedType = Type.IntegralBoundedType Type.integralType
-
---instance Type.IsIntegral Bit where
---  integralType = error "Opps" -- Type.TypeInt Type.IntegralDict
-
-instance Type.IsNum Bit where
-  numType = Type.IntegralNumType undefined -- Type.integralType
-
-instance Type.IsScalar Bit where
-  scalarType = Type.NumScalarType Type.numType
-
-
-{-
-
-newtype Bit' = Bit' Word8
-
-deriving instance Show Bit'
-deriving instance Elt Bit'
-deriving instance Typeable Bit'
-
-type instance Sugar.EltRepr' Bit' = Word8
-type instance Sugar.EltRepr Bit' = ((),Word8)
--}
-
----------------------------------------------------------------------
-
-type M a = Data.Matrix.Matrix a
-type V a = V.Vector a
-
-ldpc :: M Bit -> Int -> V Double -> IO (V Bit)
-ldpc a maxIterations orig_lam = return $ fmap hard $ loop 0 orig_ne orig_lam
-  where
-    orig_ne :: M Double
-    orig_ne = fmap (const 0) a
-
-    loop :: Int -> M Double -> V Double -> V Double
-    loop !n ne lam
-        | V.all (== 0) ans             = lam
-        | n >= maxIterations           = orig_lam
-        | otherwise                    = loop (n+1) ne' lam'
-      where
-        c_hat :: V Bit
-        c_hat = fmap hard lam
-
-        -- was bug here: needed to getCol, not getRow (which was a single element)
-        ans :: V Bit
-        ans = getCol 1 (a `Data.Matrix.multStd` colVector c_hat)
-
-        -- was bug here: V's start at index 0, not 1
-        ne' :: M Double
-        ne' = Data.Matrix.matrix (nrows orig_ne) (ncols orig_ne) $ \ (m,n) ->
-                if a ! (m,n) == 1
-                then
-                   -- was bug here: we need to cap atanh's answer
-                    -2 * atanh' (product
-                        [ tanh (- ((lam V.! (j-1) - ne ! (m,j)) / 2))
-                        | j <- [1 .. V.length orig_lam]
-                        , j /= n
-                        , a ! (m,j) == 1
-                        ])
-                else 0
-
-        -- Was bug here: needed to add the orig_lam
-        lam' :: V Double
-        lam' = V.fromList [ V.foldr (+) (orig_lam V.! (j - 1)) (getCol j ne')
-                          | j <- [1 .. V.length lam]
-                          ]
-
-
--}
